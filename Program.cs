d81312363744196661b7bd0116d5f921702e0970
Timakz <temirlan_95kz@mail.ru>
2023-03-28 21:10:13 +0600

    Homework5

--- Program.cs
+++ Program.cs
@@ -33,3 +33,76 @@
     //Console.Write("]");
     //Console.WriteLine();
 //}
+
+//Задача 36. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+
+//Console.Write("Enter the number of array elements ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//int[] randomArray = new int[a];
+
+//void mas(int a)
+//{
+//for (int i = 0; i < a; i++)
+//{
+//randomArray[i] = new Random().Next(1,9);
+//Console.Write(randomArray[i] ++);
+//}
+
+//}
+
+//int kol(int[] randomArray)
+//{
+//int sum = 0;
+//int i = 0;
+//while (i < randomArray.Length)
+//{
+//sum = sum + randomArray[i];
+//i = i + 2;
+//}
+//return sum;
+//}
+
+//mas(a);
+//Console.Write($"\nThe sum of the elements standing in odd positions {kol(randomArray)}");
+
+//Задача 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+//Console.Write("Enter the number of array elements");
+//int a = Convert.ToInt32(Console.ReadLine());
+//double[] randomArray = new double[a];
+
+//void mas(int a)
+//{
+//Random rand = new Random();
+//for (int i = 0; i < a; i++)
+//{
+//randomArray[i] = rand.NextDouble();
+//Console.Write($"{randomArray[i]:F2}");
+//}
+
+//}
+
+//double raz(double[] randomArray)
+//{
+//double min = randomArray[0];
+//double max = randomArray[0];
+//int i = 1;
+//while (i < randomArray.Length)
+//{
+//if (max<randomArray[i])
+//max = randomArray[i];
+//if (min>randomArray[i])
+//min = randomArray[i];
+//i = i + 1;
+//}
+//return max-min;
+//}
+
+//mas(a);
+//Console.Write($"\nThe difference between the maximum and minimum array elements {raz(randomArray):F2}");
